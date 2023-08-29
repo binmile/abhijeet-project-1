5aba64436658fa1fcd83c94213fefa0146cb42ad
abhijeet <abhijeet.k@binmile.com>
2023-08-29 14:12:19 +0530

    class-component added

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+
+
+import React, {Component} from "react";
+
+class Welcome extends Component{
+    render(): React.ReactNode {
+        return <h1> class Component</h1>
+    }
+}
+ export default Welcome
