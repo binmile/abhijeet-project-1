1fa272b05d1dda98079808a6fd9a5ba89dcd6dcd
abhijeet <abhijeet.k@binmile.com>
2023-09-05 14:56:05 +0530

    added lists

--- src/component/Lists.tsx
+++ src/component/Lists.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import names from '../dummyData/Dummydata'
+
 
 const Lists = () => {
-    const names=['abhijeet','niti','raman']
+    
+    const listNames= names.map((Person)=>
+    <h1> i am {Person.name}. My age is {Person.age} . my hobby is {Person.skill}</h1>
+      )
+    
+    
   return (
     <div>
-        
+       
+        {listNames}
+       
     </div>
   )
 }
---
+++ src/dummyData/Dummydata.tsx
@@ -0,0 +1,20 @@
+const names=[{
+    id: 1,
+    name: 'niti',
+    age: 29,
+    skill: 'anything',
+},
+{
+    id: 2,
+    name: 'abhijeet',
+    age: 21,
+    skill: 'nothing',
+},
+{
+    id: 3,
+    name: 'anve',
+    age: 26,
+    skill: 'thing',
+}]
+
+export default names;
