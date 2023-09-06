77ba6fb2eaf01c714617b184558e3bd2f8926958
abhijeet <abhijeet.k@binmile.com>
2023-09-06 10:36:04 +0530

    added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,26 +2,12 @@ import React, {useState,useEffect} from 'react'
 import { json } from 'stream/consumers'
 
 const Home = () => {
-    const [resourceType, setResourceType]= useState('posts')
-    const [items,setItems]= useState([])
-    useEffect(()=>{
-      fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-      .then(response => response.json())
-      .then(json => setItems(json))
-    }, [resourceType])    
+     const [windowWidth, setWindowWidth] = useState(window.innerWidth) 
+
+     useEffect(()=>)
 
   return (
-    <>
-    <div>
-        <button onClick={()=>setResourceType('posts ')}>Posts</button>
-        <button onClick={()=>setResourceType("users ")}>users</button>
-        <button onClick={()=> setResourceType('comments ')}>comments</button>
-    </div>
-    <h1>{resourceType}</h1>
-    {items.map((items)=>{
-      return <p>{JSON.stringify(items)}</p>
-    })}
-    </>
+    <div>{windowWidth}</div>
   )
 }
 
