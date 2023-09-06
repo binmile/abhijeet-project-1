2ca450bf21afce2bfa76e959a912d2126c46ebc0
abhijeet <abhijeet.k@binmile.com>
2023-09-06 14:07:07 +0530

    added the functionality to clear searchbar and add text

--- src/pages/HomePage/components/ComponentsLayout/ComponentHeading/ComponentSearchPanel/ComponentSearchBar/SearchBar.tsx
+++ src/pages/HomePage/components/ComponentsLayout/ComponentHeading/ComponentSearchPanel/ComponentSearchBar/SearchBar.tsx
@@ -1,9 +1,27 @@
+import { clear } from 'console'
 import React from 'react'
 
 const Searchbar = () => {
+
+  const [searchText, setSearchText] = React.useState('Profession, position')
+
+  const clearSearchText = () => setSearchText('')
+
+  const handleInputText = ( e: { target: { value: React.SetStateAction<string> } } ) => setSearchText(e.target.value)
+  
+  
+
   return (
     <div className='md:w-[400px] w-[230x] border-2 rounded-[30px] p-1 md:p-3 border-black'>
-         <input className='searchBarFont outline-none text-center' type="text " defaultValue={'Profession, position '}/>
+         
+
+         <input
+          className='searchBarFont outline-none text-center' 
+          type="text "
+           value={searchText}
+           onClick={clearSearchText}
+           onChange={handleInputText}
+           />
     </div>
   )
 }
