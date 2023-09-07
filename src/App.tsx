d56969749d9b038ae09ef54de7a8a14bd4ff88ea
abhijeet <abhijeet.k@binmile.com>
2023-09-07 15:03:25 +0530

    route created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Routes,Route,} from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='about' element={<About/>}></Route>
+    </Routes>
+    
+  )
 }
 
 export default App;
