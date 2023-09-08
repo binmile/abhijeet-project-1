6f62df3e3aa6fbdc2fdc0de4bd89bbe62ea320e6
abhijeet <abhijeet.k@binmile.com>
2023-09-08 14:17:42 +0530

    routers

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,17 +4,11 @@ import { NavLink } from 'react-router-dom'
 
 
 const Navbar = () => {
-
-  const navLinkStyles = ({ isActive }) => {
-    return {
-      textDecoration: isActive? 'bold' : 'normal',
-      color: isActive? 'none' : 'underline'
-    }
-  }
+ 
   return (
     // <nav className='p-3 flex gap-4 '></nav>
-    <nav className={`${navLinkStyles} p-3 flex gap-4`}>
-        <NavLink className={'text-lg'} to={'/'}>Home</NavLink>
+    <nav className={`p-3 flex gap-4`} >
+        <NavLink to={'/'}>Home</NavLink>
         <NavLink to={'about'}>About</NavLink>
 
     </nav>
